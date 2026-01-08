50862c0634b30487ce7d237becca2ea748bdb7fb
91ogsn <stefan.ostling@hotmail.com>
2026-01-08 09:43:27 +0100

    first commit

---
+++ DSO25-test-intro/CalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿namespace DSO25_test_intro;
+
+public class CalculatorTest
+{
+    [Fact]
+    public void TestAdd()
+    {
+        //given
+        Calculator Calc = new();
+
+        int a = 5;
+        int b = 2;
+        int expectedResult = 7;
+
+        //when
+        int result = Calc.Add(a, b);
+        //then
+        Assert.Equal(expectedResult, result);
+    }
+}
